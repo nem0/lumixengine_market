ff57d9d68e39a013bb1837a86cff42fa7bbec961
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-11-28 21:48:12 +0100

    some more test data

--- data/list.lua
+++ data/list.lua
@@ -1,9 +1,17 @@
 {
     {
-        name = "Animated charactesr",
+        name = "Animated characters",
         category = "Model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.png",
         install_path = "models/animated_characters",
+    },
+    {
+        name = "Survival kit",
+        category = "Model",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.png",
+        install_path = "models/survival_kit",
     }
+
 }
