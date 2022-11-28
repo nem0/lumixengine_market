1f07bb0e29326d5128b2c51ecf13c30aaaccbf61
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-11-28 22:38:00 +0100

    some new test data

--- data/list.lua
+++ data/list.lua
@@ -4,14 +4,17 @@
         category = "Model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.png",
-        install_path = "models/animated_characters",
     },
     {
         name = "Survival kit",
         category = "Model",
         path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.zip",
         thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.png",
-        install_path = "models/survival_kit",
+    },
+    {
+        name = "Tower defense kit",
+        category = "Model",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/survival_kit.png",
     }
-
 }
