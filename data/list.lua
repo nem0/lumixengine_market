abf630099a96b2a80f5f414cf957a524b1d84144
Mikulas Florek <mikulas.florek@gamedev.sk>
2022-11-28 21:39:23 +0100

    test data

---
+++ data/list.lua
@@ -0,0 +1,9 @@
+{
+    {
+        name = "Animated charactesr",
+        category = "Model",
+        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.zip",
+        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/animated-characters.png",
+        install_path = "models/animated_characters",
+    }
+}
