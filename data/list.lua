f2495e12099d97e9bbd16df35c26eceea8972aa0
Mikulas Florek <mikulas.florek@gamedev.sk>
2024-11-10 14:38:56 +0100

    cleanup

--- data/list.lua
+++ data/list.lua
@@ -1,5 +1,5 @@
 return {
-    {
+	{
 		name = "Animated characters 1",
 		tags = "model, lowpoly, animation",
 		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/kenney_animated-characters-1.zip",
@@ -173,54 +173,54 @@ return {
 		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/kenney_ui.zip",
 		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kenney/kenney_ui.png"
 	},
-    {
-        name = "Ultimate Stylized Nature",
-        tags = "model",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.png",
-    },
-    {
-        name = "Ultimate Modular Ruins",
-        tags = "model",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.png",
-    },
-    {
-        name = "Ultimate Modular Sci-Fi",
-        tags = "model",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.png",
-    },
-    {
-        name = "Platformer Pack",
-        tags = "model, lowpoly",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.png",
-    },
-    {
-        name = "Vehicle Sample",
-        tags = "scene, model, script",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.png",
-        root_install = true
-    },
-    {
-        name = "Portal effect",
-        tags = "scene, model, shader, material",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.png",
-        root_install = true
-    },
-    {
-        name = "Halloween bits",
-        tags = "model, lowpoly",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/HalloweenBits.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/HalloweenBits.png"
-    },
-    {
-        name = "Dungeon Remastered Pack ",
-        tags = "model, lowpoly",
-        path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/dugeon_remastered.zip",
-        thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/dugeon_remastered.png"
-    }
+	{
+		name = "Ultimate Stylized Nature",
+		tags = "model",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_stylized_nature.png",
+	},
+	{
+		name = "Ultimate Modular Ruins",
+		tags = "model",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_ruins.png",
+	},
+	{
+		name = "Ultimate Modular Sci-Fi",
+		tags = "model",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/ultimate_modular_scifi.png",
+	},
+	{
+		name = "Platformer Pack",
+		tags = "model, lowpoly",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/quaternius/platformer_pack.png",
+	},
+	{
+		name = "Vehicle Sample",
+		tags = "scene, model, script",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/vehicle_sample.png",
+		root_install = true
+	},
+	{
+		name = "Portal effect",
+		tags = "scene, model, shader, material",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/lumix/portal.png",
+		root_install = true
+	},
+	{
+		name = "Halloween bits",
+		tags = "model, lowpoly",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/HalloweenBits.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/HalloweenBits.png"
+	},
+	{
+		name = "Dungeon Remastered Pack ",
+		tags = "model, lowpoly",
+		path = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/dugeon_remastered.zip",
+		thumbnail = "https://raw.githubusercontent.com/nem0/lumixengine_market/master/data/kaykit/dugeon_remastered.png"
+	}
 }
